747632427cf0f1f57f12717780f10bbe3b6934b7
Sv1ne <svinina754@gmail.com>
2022-09-18 15:32:51 +0300

    update

---
+++ src/test/java/SearchTests.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Configuration.browser;
+import static com.codeborne.selenide.Configuration.holdBrowserOpen;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SearchTests {
+
+    @Test
+    void successfulSearchTest() {
+        holdBrowserOpen = true;
+        browser = "edge";
+        open("https://www.google.com/");
+        $("#W0wltc").click();
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("#search").shouldHave(text("https://selenide.org"));
+    }
+}
