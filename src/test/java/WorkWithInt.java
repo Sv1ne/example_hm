e5243acbb509f6691a6c06c4741984eb9079fbae
Sv1ne <svinina754@gmail.com>
2022-10-20 18:32:12 +0300

    add WorkWithInt.java

---
+++ src/test/java/WorkWithInt.java
@@ -0,0 +1,15 @@
+public class WorkWithInt {
+    public static void main(String[] args) {
+        int a = Integer.MAX_VALUE+1;
+        int b = Integer.MIN_VALUE-1;
+        System.out.println(a);
+        System.out.println(b);
+
+        System.out.println(a+b);
+
+
+
+
+
+    }
+}
